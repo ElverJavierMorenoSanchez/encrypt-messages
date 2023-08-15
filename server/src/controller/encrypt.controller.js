3808ca75d8ec1b119cb8af03a441d979326eea01
Javier Moreno <84688202+ElverJavierMorenoSanchez@users.noreply.github.com>
2023-08-15 15:32:50 -0500

    fix bug

--- server/src/controller/encrypt.controller.js
+++ server/src/controller/encrypt.controller.js
@@ -42,30 +42,26 @@ export const dencryptFile = async (req, res) => {
     console.log(newName);
 
     setTimeout(() => {
-      fs.readFile(`./src/uploads/${filename}`, (err, file) => {
+      fs.readFile(`./uploads/${filename}`, (err, file) => {
         if (err) return console.log(err);
 
         const decryptedFile = decrypt(file, _secret);
-        fs.writeFile(
-          `./src/uploads/${newName}`,
-          decryptedFile,
-          (error, _file) => {
-            if (error)
-              return res
-                .status(404)
-                .json({ message: "Error al desencriptar el archivo" });
+        fs.writeFile(`./uploads/${newName}`, decryptedFile, (error, _file) => {
+          if (error)
+            return res
+              .status(404)
+              .json({ message: "Error al desencriptar el archivo" });
 
-            const file = path.resolve(`./src/uploads/${newName}`);
+          const file = path.resolve(`./uploads/${newName}`);
 
-            res.status(200).download(file);
+          res.status(200).download(file);
 
-            setTimeout(() => {
-              fs.unlinkSync(`./src/uploads/${newName}`, (error) => {
-                console.log(error.message);
-              });
+          setTimeout(() => {
+            fs.unlinkSync(`./uploads/${newName}`, (error) => {
+              console.log(error.message);
             });
-          }
-        );
+          });
+        });
       });
     }, 5000);
   } catch (error) {
@@ -75,7 +71,7 @@ export const dencryptFile = async (req, res) => {
 
 export const getFile = async (req, res) => {
   const { filename } = req.params;
-  const file = path.resolve(`./src/uploads/${filename}`);
+  const file = path.resolve(`./uploads/${filename}`);
   res.download(file);
 };
 
@@ -92,22 +88,22 @@ export const encryptFile = async (req, res) => {
     .substring(0, 32);
 
   try {
-    EDFile.mv(`./src/uploads/${EDFile.name}`, (err) => {
+    EDFile.mv(`./uploads/${EDFile.name}`, (err) => {
       if (err) return res.status(500).send({ message: err });
     });
 
     setTimeout(() => {
-      fs.readFile(`./src/uploads/${EDFile.name}`, (err, file) => {
+      fs.readFile(`./uploads/${EDFile.name}`, (err, file) => {
         if (err) return console.log(err);
 
         const encryptedFile = encrypt(file, _secret);
 
         fs.writeFile(
-          `./src/uploads/encriptado-${EDFile.name}`,
+          `./uploads/encriptado-${EDFile.name}`,
           encryptedFile,
           (error, _) => {
             setTimeout(() => {
-              fs.unlinkSync(`./src/uploads/${EDFile.name}`, (error) => {
+              fs.unlinkSync(`./uploads/${EDFile.name}`, (error) => {
                 console.log(error.message);
               });
             });
